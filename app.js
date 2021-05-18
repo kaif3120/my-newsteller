778a67a2681efecb9659eb1215ae3dce23634c5d
Mohd kaif <shaikhkaif3120@gmail.com>
2021-05-18 21:18:34 +0530

    let's see

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.post("/failure",function(req,res){
 
 
 
-app.listen(process.env.PORT, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("your server is running at port 3000");
 })
 
